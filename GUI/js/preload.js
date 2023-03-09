0943230c4f994d0d038915129532f49f3af67f48
Hariprasad M.S <61413416+hariprasad-ms@users.noreply.github.com>
2023-03-09 22:54:14 +0530

    Update preload.js

--- GUI/js/preload.js
+++ GUI/js/preload.js
@@ -1,5 +1,12 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+
+// DESTINY Autonimous Artificial Intelligence Programme (Python 3).
+// Copyright (c) 2012 - End Of Times | OUR DESTINY (A.A.I) Research And Development Labs,Inc.
+// All rights reserved. This software or any portion thereof may not be reproduced or used in any manner whatsoever without the development permission of the ODRE Labs, Inc.
+// Permission for the development of the software is Authorized ONLY for the developers of ODRD Labs, Inc.
+// Author: OUR DESTINY (A.A.I) Research And Development Labs <hariprasad@ODRDLabs>|<ribin@ODRDLabs>'''
+
 console.log("preload started");
 let {PythonShell} = require('python-shell')
  let options = {
