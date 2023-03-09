9ee6b6dfe03bda437670ab48bb02021b4b49db81
Hariprasad M.S <61413416+hariprasad-ms@users.noreply.github.com>
2023-03-09 22:51:51 +0530

    Update Destiny_update_db.py

--- Destiny_Mainframe/Destiny_update_db.py
+++ Destiny_Mainframe/Destiny_update_db.py
@@ -1,4 +1,11 @@
+'''DESTINY Autonimous Artificial Intelligence Programme (Python 3).
+Copyright (c) 2012 - End Of Times | OUR DESTINY (A.A.I) Research And Development Labs,Inc.
+All rights reserved. This software or any portion thereof may not be reproduced or used in any manner whatsoever without the development permission of the ODRE Labs, Inc.
+Permission for the development of the software is Authorized ONLY for the developers of ODRD Labs, Inc.
+Author: OUR DESTINY (A.A.I) Research And Development Labs <hariprasad@ODRDLabs>|<ribin@ODRDLabs>'''
+
 import sqlite3 as sqlite
+
 def update(in1,out1):
     conn = sqlite.connect('./GUI/js/returnResp.sqlite')
     cur = conn.cursor()
