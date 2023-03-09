8ba61c25d1ccf6f68d66204bc61f4f99181f3e54
Hariprasad M.S <61413416+hariprasad-ms@users.noreply.github.com>
2023-03-09 22:43:13 +0530

    Update Destiny_Intro.py

--- Destiny_Mainframe/Destiny_Intro.py
+++ Destiny_Mainframe/Destiny_Intro.py
@@ -1,3 +1,10 @@
+'''DESTINY Autonimous Artificial Intelligence Programme (Python 3).
+Copyright (c) 2012 - End Of Times | OUR DESTINY (A.A.I) Research And Development Labs,Inc.
+All rights reserved. This software or any portion thereof may not be reproduced or used in any manner whatsoever without the development permission of the ODRE Labs, Inc.
+Permission for the development of the software is Authorized ONLY for the developers of ODRD Labs, Inc.
+Author: OUR DESTINY (A.A.I) Research And Development Labs <hariprasad@ODRDLabs>|<ribin@ODRDLabs>'''
+
+
 import Destiny_Threader, Destiny_Protocols
 import sqlite3 as sqlite
 
