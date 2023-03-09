2c2e6492a78569b8172c4054914e73d7d09f3b02
Hariprasad M.S <61413416+hariprasad-ms@users.noreply.github.com>
2023-03-09 22:42:43 +0530

    Update DSD.py

--- Destiny_Mainframe/Destiny_Speech_Drive/DSD.py
+++ Destiny_Mainframe/Destiny_Speech_Drive/DSD.py
@@ -1,3 +1,9 @@
+'''DESTINY Autonimous Artificial Intelligence Programme (Python 3).
+Copyright (c) 2012 - End Of Times | OUR DESTINY (A.A.I) Research And Development Labs,Inc.
+All rights reserved. This software or any portion thereof may not be reproduced or used in any manner whatsoever without the development permission of the ODRE Labs, Inc.
+Permission for the development of the software is Authorized ONLY for the developers of ODRD Labs, Inc.
+Author: OUR DESTINY (A.A.I) Research And Development Labs <hariprasad@ODRDLabs>|<ribin@ODRDLabs>'''
+
 from Speech_Drive_Dependencies import pyttsx3
 
 destiny_engine = pyttsx3.init()
